b9f7620f2778fea5986e89f0cb7901747641a594
Samuel dev <osorio.felipe@email.com>
2025-06-18 01:31:19 -0500

    Segundo deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/jrTestBMR/', // nombre del repositorio importante para GitHub Pages
+  build: { // Configuración de la carpeta de salida
+    outDir: 'docs'
+  },
   plugins: [react()],
-})
+});
+
